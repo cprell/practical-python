a929dc3a5acb0ad509c5e044f63c152e4a81ed47
Christian Prell <chr.prell@googlemail.com>
2020-07-26 16:03:37 +0200

    first excercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+num_bounces = 0
+
+while num_bounces < 10:
+    height = height * 0.6
+    num_bounces = num_bounces + 1
+    print(num_bounces, round(height, 4))
