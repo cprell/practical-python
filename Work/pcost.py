92411905a9c6649ac46ba68ea94273a0e0ce184b
Christian Prell <chr.prell@googlemail.com>
2020-08-01 13:59:58 +0200

    refactoring

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,15 @@
 # pcost.py
-#
-# Exercise 1.27
 
-total_cost = 0
 
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f).split(',')
-for line in f:
-    row = line.split(',')
-    total_cost = total_cost + int(row[1]) * float(row[2])
+def portfolio_cost(filename):
+    total_cost = 0.0
+    f = open(filename, 'rt')
+    next(f)
+    for line in f:
+        row = line.split(',')
+        total_cost = total_cost + int(row[1]) * float(row[2])
+    return total_cost
 
-print('Total cost', total_cost)
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost', cost)
