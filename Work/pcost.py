0e3404655f1660d4a7db594bf83c47448b6f9dbc
Christian Prell <chr.prell@googlemail.com>
2020-08-01 14:32:36 +0200

    read inputs

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,32 @@
 # pcost.py
 
+import csv
+import sys
+
 
 def portfolio_cost(filename):
     total_cost = 0.0
     f = open(filename, 'rt')
-    next(f)
-    for line in f:
-        row = line.split(',')
+    rows = csv.reader(f)
+    next(rows)
+    for row in rows:
+        print(row)
         try:
             shares = int(row[1])
             price = float(row[2])
+            total_cost = total_cost + shares * price
         except ValueError:
-            print("Couldn't parse", line)
-        total_cost = total_cost + shares * price
+            print("Couldn't parse", row)
+
+    f.close()
     return total_cost
 
 
-cost = portfolio_cost('Data/missing.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+
+cost = portfolio_cost(filename)
 print('Total cost', cost)
