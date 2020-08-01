9e5ecaddeff19d8c6169ff2d1076cb2842cbaeb6
Christian Prell <chr.prell@googlemail.com>
2020-08-01 13:32:39 +0200

    1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+for line in f:
+    row = line.split(',')
+    total_cost = total_cost + int(row[1]) * float(row[2])
+
+print('Total cost', total_cost)
